d7bb611b179c9c096759a7a0ee5221a512d0a067
Mostafa Hesham <mostafahesham5500@gmail.com>
2022-05-10 05:07:28 +0200

    react-store

--- src/component/nav/nav.js
+++ src/component/nav/nav.js
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import { Navbar,Container,Nav } from 'react-bootstrap';
 import "./nav.css";
 
+import src from '../../../package.json';
+
 function Navbarr(){
     return(
         <div className='nav'>
@@ -11,11 +13,11 @@ function Navbarr(){
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to="/home">Home</Link>
-                        <Link to="/about">ABOUT US</Link>
-                        <Link to="/delivery">DELIVERY</Link>
-                        <Link to="/blog">BLOG</Link>
-                        <Link to="/featured">FEATURED</Link>
+                        <Link to={src.homepage}>Home</Link>
+                        <Link to={src.homepage + "about"}>ABOUT US</Link>
+                        <Link to={src.homepage + "delivery"}>DELIVERY</Link>
+                        <Link to={src.homepage + "blog"}>BLOG</Link>
+                        <Link to={src.homepage + "featured"}>FEATURED</Link>
                     </Nav>
                     </Navbar.Collapse>
                 </Container>
