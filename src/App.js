5d5cff42b73c4b75c9827dd2c879fe7270dfb5d2
Mostafa Hesham <mostafahesham5500@gmail.com>
2022-05-10 05:32:01 +0200

    hh

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ function App() {
         <Logo/>
         <Nav/>
           <Routes>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
-            <Route path="/blog" element={<Blog/>}/>
-            <Route path="/delivery" element={<Del/>}/>
-            <Route path="/featured" element={<Feutured/>}/>
-            <Route path="/cart" element={<Cart/>}/>
+            <Route path="home" element={<Home/>}/>
+            <Route path="" element={<Home/>}/>
+            <Route path="about" element={<About/>}/>
+            <Route path="blog" element={<Blog/>}/>
+            <Route path="delivery" element={<Del/>}/>
+            <Route path="featured" element={<Feutured/>}/>
+            <Route path="cart" element={<Cart/>}/>
           </Routes>
         </BrowserRouter>
         <Footer/>
